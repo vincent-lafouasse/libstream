13c66497f00af95797adad1910a4de5077a185f9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-08 12:25:57 +0100

    update generator to generate i24, u24, f32 and f64 as well

--- src/generateImplementation.py
+++ src/generateImplementation.py
@@ -1,40 +1,45 @@
 import itertools
 
 
-def implement_read_integer(bitdepth, endian, mode, signedness):
-    if signedness == "signed":
-        typename = f"uint{bitdepth}_t"
-        shortType = f"U{bitdepth}"
-    else:
-        typename = f"int{bitdepth}_t"
-        shortType = f"I{bitdepth}"
-
+def implement_read_integer(mode, shortType, containerType, size, endian):
     functionName = f"reader_{mode}{shortType}_{endian}"
     bitcastCall = f"bitcast{shortType}_{endian}(slice.slice)"
 
-    sizeof = bitdepth // 8
-    print(f"ReadStatus {functionName}(Reader* reader, {typename}* out)")
+    print(f"ReadStatus {functionName}(Reader* reader, {containerType}* out)")
     print("{")
     print("    Slice slice;")
-    print(f"    TRY(reader_peekSlice(reader, {sizeof}, &slice));")
+    print(f"    TRY(reader_peekSlice(reader, {size}, &slice));")
     print("")
     print(f"    *out = {bitcastCall};")
     if mode == "peek":
         print("    return ReadStatus_Ok;")
     else:
-        print(f"    return reader_skip(reader, {sizeof});")
+        print(f"    return reader_skip(reader, {size});")
     print("}")
     print("")
 
 
 if __name__ == "__main__":
     bitdepths = [16, 32, 64]
+    signedness = [("U", "uint"), ("I", "int")]
+
+    types = [
+        [f"{prefix}{bitdepth}", f"{c_type_prefix}{bitdepth}_t", bitdepth // 8]
+        for bitdepth in bitdepths
+        for prefix, c_type_prefix in signedness
+    ]
+
+    types += [
+        ["I24", "int32_t", 3],
+        ["U24", "uint32_t", 3],
+        ["F32", "float", 4],
+        ["F64", "double", 8],
+    ]
+
     # stringly typed development
     endianness = ["LE", "BE"]
     modes = ["peek", "take"]
     signedness = ["signed", "unsigned"]
 
-    for bitdepth, endian, mode, sign in itertools.product(
-        bitdepths, endianness, modes, signedness
-    ):
-        implement_read_integer(bitdepth, endian, mode, sign)
+    for T, endian, mode in itertools.product(types, endianness, modes):
+        implement_read_integer(mode, T[0], T[1], T[2], endian)
