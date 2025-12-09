3e59f3922e20b1282f537ccf7d71640b363d8c78
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-09 22:19:53 +0100

    remove outdated test

--- test/BitReaderTest/BitReaderTest.cpp
+++ test/BitReaderTest/BitReaderTest.cpp
@@ -12,27 +12,12 @@
 
 TEST(BitReader, Init_StateAndOffset)
 {
-    MockReader mockReader("ABC"); 
+    MockReader mockReader("ABC"); // irrelevant
     Reader reader = mockReaderInterface(&mockReader);
 
     BitReader br = bitreader_init(&reader);
-
-    ASSERT_EQ(br.subOffset, 0u);
-    ASSERT_TRUE(bitreader_isByteAligned(&br));
 
     ASSERT_EQ(bitreader_getByteOffset(&br), 0u);
-    ASSERT_EQ(bitreader_getBitOffset(&br), 0u);
-}
-
-TEST(BitReader, GetByteOffset_MatchesReaderSkip)
-{
-    MockReader mockReader("ABCDEFGHIJ"); 
-    Reader reader = mockReaderInterface(&mockReader);
-    BitReader br = bitreader_init(&reader);
-
-    // simulate a read
-    reader_skip(&reader, 5); 
-
-    ASSERT_EQ(bitreader_getByteOffset(&br), 5u);
-    ASSERT_EQ(bitreader_getBitOffset(&br), 40u); // 5 bytes
+    ASSERT_EQ(br.subOffset, 0u);
+    ASSERT_TRUE(bitreader_isByteAligned(&br));
 }
