056e2c1e20d723f7adc301f8dca982642292956a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-09 17:45:00 +0100

    init BitReader test and restructure

---
+++ test/BitReaderTest/BitReaderTest.cpp
@@ -0,0 +1,19 @@
+#include <cstdint>
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include "libstream/BitReader.h"
+#include "libstream/Reader.h"
+#include "MockReader.hpp"
+
+#define ASSERT_IS_OK(status) ASSERT_EQ(status, LibStream_ReadStatus_Ok)
+#define ASSERT_IS_EOF(status) \
+    ASSERT_EQ(status, LibStream_ReadStatus_UnexpectedEOF)
+
+TEST(BitReader, Dummy)
+{
+    MockReader mockReader("\x44");
+    Reader byteReader = mockReaderInterface(&mockReader);
+    BitReader reader = bitreader_init(&byteReader);
+}
