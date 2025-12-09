dd7800d94131833c33a8684488b82564ef56b774
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-12-09 18:00:01 +0100

    format

--- test/BitReaderTest/BitReaderTest.cpp
+++ test/BitReaderTest/BitReaderTest.cpp
@@ -3,9 +3,9 @@
 
 #include "gtest/gtest.h"
 
+#include "MockReader.hpp"
 #include "libstream/BitReader.h"
 #include "libstream/Reader.h"
-#include "MockReader.hpp"
 
 #define ASSERT_IS_OK(status) ASSERT_EQ(status, LibStream_ReadStatus_Ok)
 #define ASSERT_IS_EOF(status) \
